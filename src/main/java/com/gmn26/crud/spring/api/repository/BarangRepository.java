415d0fd3c043819e24eb1b8cafdccabcd145212d
gmn26 <ghazynovrial@gmail.com>
2025-01-31 01:35:45 +0700

    update : add queryDSL configuration

--- src/main/java/com/gmn26/crud/spring/api/repository/BarangRepository.java
+++ src/main/java/com/gmn26/crud/spring/api/repository/BarangRepository.java
@@ -3,11 +3,12 @@ package com.gmn26.crud.spring.api.repository;
 import com.gmn26.crud.spring.api.entity.Barang;
 //import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface BarangRepository extends JpaRepository<Barang, Long> {
+public interface BarangRepository extends JpaRepository<Barang, Long>, QuerydslPredicateExecutor<Barang> {
     List<Barang> findAll();
 }
