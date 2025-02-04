f091afc846d9e20a3453c7de48b0a758dc362006
gmn26 <ghazynovrial@gmail.com>
2025-02-05 00:53:49 +0700

    refactor : fix throwing error if login invalid, make it into better ResponseEntity

--- src/main/java/com/gmn26/crud/spring/api/service/UserServiceImpl.java
+++ src/main/java/com/gmn26/crud/spring/api/service/UserServiceImpl.java
@@ -43,7 +43,7 @@ public class UserServiceImpl implements UserService {
 
         } catch (AuthenticationException e) {
             log.error("Authentication failed error: {}", e.getMessage());
-            throw e;
+            return null;
         }
     }
 }
